b1ee8a3966c9bc2716e0e2969cdfbf7a91957186
= <jjjacksonoh@gmail.com>
2021-05-11 22:55:42 -0500

    updated config.js

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const env = process.env;
 
 const dbConfig = {
     database: `mongodb+srv://${env.DB_USERNAME}:${env.DB_PASSWORD}@${env.DB_HOST}/${env.DB_NAME}?retryWrites=true&w=majority`,
-    host: "http://localhost",
+    host: "https://cs-396-hw03-jackson.herokuapp.com/",
     mongoConfig: {
         useCreateIndex: true,
         useFindAndModify: false,
